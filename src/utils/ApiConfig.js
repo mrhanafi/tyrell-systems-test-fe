a92b7954b965602d218affab35bf7e0f3a44898e
mrhanafi <muhdhanafirahman@gmail.com>
2025-06-19 17:18:55 +0800

    add prod url

--- src/utils/ApiConfig.js
+++ src/utils/ApiConfig.js
@@ -1,1 +1,5 @@
-export const URL = 'http://localhost:8000/api'
+// dev url
+// export const URL = 'http://localhost:8000/api'
+
+// prod url
+export const URL = 'https://tyrellbe.hanafirahman.com/api'
